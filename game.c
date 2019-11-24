70dda9a048c0876a2b0e4d58a951d824242992aa
Cyber Anubis <33065305+NidalFikri@users.noreply.github.com>
2019-11-24 19:40:08 +0200

    Update game.c
    new functions added.

--- game.c
+++ game.c
@@ -55,9 +55,46 @@ int get_player_data()
 	return 1;
 }
 
-// This is the new user registration function. 
+// This is the new user registration function.
 // It will create a new player account and append it to the file.
 void register_new_palyer ()
 {
-	
+	int fd;
+	printf("-=-={ New Player Registration }=-=-\n");
+	printf("Enter your name: ");
+	input_name();
+
+	//Initializing the user data.
+	player.uid = getuid();
+	player.highscore = player.credits = 100;
+
+	fd = open(DATAFILE, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+	if (fd ==-1) die("in registering new player.");
+
+	//Saving the player data into the file database.
+	write(fd, &player, sizeof(struct user));
+	close(fd);
+
+	printf("\nWelcome to the Game of Chance %s.\n", player.name);
+	printf("You have been given %u credits.\n", player.credits);
+}
+
+
+// This function is used to input the player name, since
+// scanf("%s", &whatever) will stop input at the first space.
+void input_name()
+{
+	char *name_ptr, input_char ='\n';
+
+	//Flush any leftover newlines.
+	while (input_char == '\n') scanf("%c",&input_char);
+
+	name_ptr = (char *)&(player.name);	//points to the name attribute in the player struct.
+	while (input_char != '\n')
+	{
+		*name_ptr=input_char;		//get the char.
+		scanf("%c",&input_char);	//take new input char.
+		name_ptr++;					//++ the pointer position.
+	}
+	*name_ptr=0;	//NULL terminate the string.
 }
